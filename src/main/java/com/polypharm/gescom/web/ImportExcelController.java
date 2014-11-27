bbb815193d9a2c54aa4b34560279970012b83299
guymoyo <guymoyo@yahoo.fr>
2014-11-27 11:46:41 +0100

    download sheet

--- src/main/java/com/polypharm/gescom/web/ImportExcelController.java
+++ src/main/java/com/polypharm/gescom/web/ImportExcelController.java
@@ -3,11 +3,17 @@ package com.polypharm.gescom.web;
 import java.io.IOException;
 import java.io.InputStream;
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.polypharm.gescom.sheetLoader.DataSheetLoader;
@@ -33,12 +39,29 @@ public class ImportExcelController {
 		return "index";
 	}
 
-	@RequestMapping("/excelfile1")
-	public String importExcelFile1(){
+//	@RequestMapping("/excelfile1")
+//	public String importExcelFile1(){
+//		
+//		InputStream stream = dataSheetLoader.loadFile();
+//		dataSheetLoader.loadDataSheet(stream);
+//		
+//		return "index";
+//	}
+	
+	@RequestMapping(value="/download", produces= {"application/vnd.ms-excel"})
+	@ResponseBody
+	public void downloadExcelFile1( HttpServletRequest httpServletRequest
+   		,HttpServletResponse response) throws IOException{
 		
-		InputStream stream = dataSheetLoader.loadFile();
-		dataSheetLoader.loadDataSheet(stream);
+		String dataSheetFile = "polypharm.xls";
+		InputStream stream = this.getClass().getResourceAsStream("/" + dataSheetFile);
 		
-		return "index";
+		ServletOutputStream os = response.getOutputStream();
+		response.setContentType("application/vnd.ms-excel");
+		response.setHeader("Content-disposition","attachment; filename=polypharm.xls");
+		IOUtils.copy(stream, os);
+		
+		return ;
 	}
+		
 }
